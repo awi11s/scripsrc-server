c6fa5133aa729af564ec15f578fd7cb15cb1b74c
Austin Wills <austinwills98@gmail.com>
2021-11-16 11:09:38 -0600

    api landing page

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import resolvers from './apollo/resolvers/index.js';
 import typeDefs from './apollo/typeDefs.js';
+import { 
+    ApolloServerPluginLandingPageLocalDefault, 
+    ApolloServerPluginLandingPageProductionDefault 
+} from 'apollo-server-core';
 
 dotenv.config();
 
 let MONGO;
 
 if (process.env.NODE_ENV === "development") {
+
     MONGO = process.env.MONGODB;
     console.log("you are in development")
+
 } else {
     MONGO = process.env.MONGO_URL;
 }
@@ -20,7 +26,15 @@ if (process.env.NODE_ENV === "development") {
 const server = new ApolloServer({ 
     typeDefs, 
     resolvers,
-    context: ({ req }) => ({ req }) 
+    context: ({ req }) => ({ req }),
+    plugins: [
+        process.env.NODE_ENV === "production"
+        ? ApolloServerPluginLandingPageProductionDefault({
+            graphRef: "my-graph-id@my-graph-variant",
+            footer: false,
+        })
+        : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+    ],
 });
 
 mongoose
